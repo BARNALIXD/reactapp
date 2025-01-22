c353c578b8e78958e1ef9f54be44924309f6f9d5
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-22 10:41:53 +0530

    ROOT UPGRADE

--- src/App.js
+++ src/App.js
@@ -8,3 +8,13 @@ function Hello(props) {
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<Hello />);
+
+
+// Before
+import ReactDOM from 'react-dom';
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// After
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
