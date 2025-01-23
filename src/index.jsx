3133d22decf6ba1165455323e0dac82417e6e4d3
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-23 10:27:28 +0530

    To pass an argument to an event handler, use an arrow function

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
-function Football() {
-    const shoot = () => {
-        alert("Great shot!");
-    }
 
-    return (
-        <button onClick={shoot}>Take the shot!</button>
-    );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Football() {
+  const shoot = (a) => {
+    alert(a);
+  }
+
+  return (
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+  );
 }
 
-const root = ReactDOM.createElement(document.getElementById("root"));
-root.render (<Football  />);
-
-//put the shoot function in the football component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Football />);
