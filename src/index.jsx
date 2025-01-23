eb014110d3ae70cb9fb3f9f23dc3749a8960f4ec
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-23 10:09:29 +0530

    REACT EVENTS

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+function Football() {
+    const shoot = () => {
+        alert("Great shot!");
+    }
+
+    return (
+        <button onClick={shoot}>Take the shot!</button>
+    );
+}
+
+const root = ReactDOM.createElement(document.getElementById("root"));
+root.render (<Football  />);
