f6b972b2d8b2c89faccc8e2aee28dc612d6db523
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-23 10:38:14 +0530

    React Event Object

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
-
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
 function Football() {
-  const shoot = (a) => {
-    alert(a);
+    const shoot = (a, b) => {
+      alert(b.type);
+      /*
+      'b' represents the React event that triggered the function,
+      in this case the 'click' event
+      */
+    }
+  
+    return (
+      <button onClick={(event) => shoot("Goal!", event)}>Take the shot!</button>
+    );
   }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Football />);
 
-  return (
-    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+  //Arrow Function: Sending the event object manually:
