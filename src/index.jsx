e56d60f6b03b13dbc51856c7e42e70aa4ed2f525
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-24 20:48:19 +0530

    In React, you will render lists with some type of loop.
    The JavaScript map() array method is generally the preferred method.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-//condition ? true : false
+const cars = [
+  { id: 1, brand: 'Ford' },
+  { id: 2, brand: 'BMW' },
+  { id: 3, brand: 'Audi' }
+];
 
-function Goal(props) {
-  const isGoal = props.isGoal;
+function Garage() {
   return (
     <>
-      { isGoal ? <MadeGoal/> : <MissedGoal/> }
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
     </>
   );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Goal isGoal={false} />);
