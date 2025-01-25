46ea051b6179d71811cf69184cb27ae28c11e166
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-26 02:14:01 +0530

    React Lists

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,22 @@
-function MyForm() {
-  return (
-    <form>
-      <label>Enter your name:
-        <input type="text" />
-      </label>
-    </form>
-  )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
 
+//car component
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+//garage component
+function Garage() {
+  const cars = ['Ford', 'BMW', 'Audi'];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car brand={car} />)}
+      </ul>
+    </>
+  );
+}
+
+//render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage />);
