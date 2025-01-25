62a3a75bc3406094d4de915995da9c8af173432f
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-26 02:09:15 +0530

    Adding Forms in React

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
-const cars = [
-  { id: 1, brand: 'Ford' },
-  { id: 2, brand: 'BMW' },
-  { id: 3, brand: 'Audi' }
-];
-
-function Garage() {
+function MyForm() {
   return (
-    <>
-      <h1>Who lives in my garage?</h1>
-      <ul>
-        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
-      </ul>
-    </>
-  );
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+  )
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
+
