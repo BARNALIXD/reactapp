fa6bf0610891a21cb36bcc311d61558406a8c134
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-01-26 02:31:10 +0530

    Keys

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,22 @@
-
-//car component
 function Car(props) {
   return <li>I am a { props.brand }</li>;
 }
 
-//garage component
 function Garage() {
-  const cars = ['Ford', 'BMW', 'Audi'];
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
   return (
     <>
       <h1>Who lives in my garage?</h1>
       <ul>
-        {cars.map((car) => <Car brand={car} />)}
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
       </ul>
     </>
   );
 }
 
-//render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Garage />);
